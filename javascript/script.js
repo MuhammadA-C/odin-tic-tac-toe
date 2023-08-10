99b7dee20c8f91f802d63684a21714b48bfb9fa0
MuhammadA-C <phantomganhdi@gmail.com>
2023-08-09 18:26:25 -0700

    Getting started
    Started with creating the game board module that is going to store the game board.
    
    Note: The game board is going to be an array with 9 slots.

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+//Create a GameBoard Object that stores the gameboard array
+//Create players object
+//Create object that controls the flow of the game
+//Use a module when you only ever need one of something
+//Use a factory function when you need more than one
+
+/////////////////////////////////////////////////////////////
+
+//Player Factory Function
+const playerFactory = () => {};
+
+//gameBoard Module
+const GameBoard = (() => {
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+  let resetGameBoard = () => {
+    for (let i = 0; i < gameBoard.length; i++) {
+      gameBoard[i] = "";
+    }
+  };
+
+  return { gameBoard, resetGameBoard };
+})();
+
+/////////////////////////////////////////////////////////////
