fae1ca6fdfa37f444110e4fe1392aba00f093eb3
MuhammadA-C <phantomganhdi@gmail.com>
2023-08-09 20:01:29 -0700

    Players Take Turns
    Created the code for the players to take turns.

--- javascript/script.js
+++ javascript/script.js
@@ -6,23 +6,7 @@
 
 //Player Factory Function
 const playerFactory = (symbol) => {
-  let pick = () => {
-    const gameBoardDOM = document.querySelector("#game-board");
-
-    gameBoardDOM.addEventListener("click", (e) => {
-      if (e.target.className == "grid-cell") {
-        if (e.target.textContent == "") {
-          //update game board DOM cell
-          e.target.textContent = symbol;
-          //Update game board array
-          let index = e.target.getAttribute("data-cell");
-          GameBoard.gameBoard[index] = symbol;
-        }
-      }
-    });
-  };
-
-  return { symbol, pick };
+  return { symbol };
 };
 
 //gameBoard Module
@@ -38,11 +22,34 @@ const GameBoard = (() => {
   return { gameBoard, resetGameBoard };
 })();
 
-const Game = (() => {
-  const player1 = playerFactory("X");
-  const player2 = playerFactory("O");
-
-  return {};
-})();
-
 /////////////////////////////////////////////////////////////
+const gameBoardDOM = document.querySelector("#game-board");
+const player1 = playerFactory("X");
+const player2 = playerFactory("O");
+let player1Went = false;
+
+gameBoardDOM.addEventListener("click", (e) => {
+  if (!player1Went) {
+    if (e.target.className == "grid-cell") {
+      if (e.target.textContent == "") {
+        //update game board DOM cell
+        e.target.textContent = player1.symbol;
+        //Update game board array
+        let index = e.target.getAttribute("data-cell");
+        GameBoard.gameBoard[index] = player1.symbol;
+        player1Went = true;
+      }
+    }
+  } else {
+    if (e.target.className == "grid-cell") {
+      if (e.target.textContent == "") {
+        //update game board DOM cell
+        e.target.textContent = player2.symbol;
+        //Update game board array
+        let index = e.target.getAttribute("data-cell");
+        GameBoard.gameBoard[index] = player2.symbol;
+        player1Went = false;
+      }
+    }
+  }
+});
